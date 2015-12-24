1ca90f105b1a8704456a69d54929ac69d9aaa2b3
Mandish Shah <shahmandish95@gmail.com>
2015-12-24 16:26:02 -0500

    Add Client API Service

---
+++ src/app/components/clientsAPI/clientsAPI.service.js
@@ -0,0 +1,17 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('myClients')
+    .factory('clientsAPI', clientsAPI);
+
+  /** @ngInject */
+  function clientsAPI($log, $http) {
+    var baseURL = 'http://localhost:8080/api';
+    return {
+      getClients: function () {
+        return $http.get(baseURL + '/clients');
+      }
+    };
+  }
+})();
