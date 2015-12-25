c00a8e5e5a3fa2434df2c9ef7c86574b9193b98e
Mandish Shah <shahmandish95@gmail.com>
2015-12-25 01:02:04 -0500

    Display notification message upon addClient request

--- src/app/addClient/addClient.controller.js
+++ src/app/addClient/addClient.controller.js
@@ -8,10 +8,10 @@
   /** @ngInject */
   function AddClientController($log, clientsAPI) {
     var vm = this;
-    vm.saveClient = function (client) {
+    vm.saveClient = function(client) {
       $log.debug('Request to add a new Client');
-      clientsAPI.AddClient(client).then(function (response) {
-        console.log(response);
+      clientsAPI.AddClient(client).then(function(response) {
+        vm.notificationMessage = response.status === 200 ? 'Client was added successfully!' : 'There was error adding the client!';
       });
     };
   }
